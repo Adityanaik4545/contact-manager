0e182cde7fb30bc57f069219ddb416117a6eadfb
Adityanaik4545 <adityank23@outlook.com>
2025-06-05 20:46:32 +0530

    resolved bug

--- contact-backend/app.js
+++ contact-backend/app.js
@@ -1,10 +1,17 @@
-const express=require("express")
-const dotenv=require("dotenv").config()
-const contact=require("./routes/contactRoute")
-const app=express()
-const port=process.env.PORT || 3000;
-app.use("/api/contacts",contact)
-app.listen(port,()=>{
-    console.log("server started on",port);
-    
+const express = require("express")
+const connectdb = require("./config/connectiondb")
+const errorHandler = require("./middleware/errorHandler")
+const dotenv = require("dotenv").config()
+const contact = require("./routes/contactRoute")
+const user = require("./routes/userRoutes")
+connectdb();
+const app = express()
+app.use(express.json())
+const port = process.env.PORT || 3000;
+app.use("/api/contacts", contact)
+app.use("/api/user", user)
+app.use(errorHandler)
+app.listen(port, () => {
+    console.log("server started on", port);
+
 })
